347e25356fe753f872d2e5844c9ef322d7ec67ad
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2019-08-28 21:40:21 +0530

    feat: added clicks

--- app/src/main/java/com/developers/coroutinesrx/state/UIBindingActivity.kt
+++ app/src/main/java/com/developers/coroutinesrx/state/UIBindingActivity.kt
@@ -6,9 +6,16 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.developers.coroutinesrx.R
 import com.developers.coroutinesrx.databinding.ActivityUibindingBinding
-import io.reactivex.Observable
+import com.developers.coroutinesrx.utils.clicks
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class UIBindingActivity : AppCompatActivity() {
+
+    private val job = Job()
+    private val dispatchers = Dispatchers.IO + job
+    private val coroutineScope = CoroutineScope(job + dispatchers)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +25,9 @@ class UIBindingActivity : AppCompatActivity() {
         )
         val uiBindingViewModel = ViewModelProviders.of(this).get(UiBindingViewModel::class.java)
         viewBinding.uiViewModel = uiBindingViewModel
+        viewBinding.takeButton.clicks(coroutineScope) {
 
+        }
     }
 
 
--- app/src/main/java/com/developers/coroutinesrx/utils/Extensions.kt
+++ app/src/main/java/com/developers/coroutinesrx/utils/Extensions.kt
@@ -3,9 +3,10 @@ package com.developers.coroutinesrx.utils
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import android.view.View
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.actor
 
 fun <T> Context.intentTo(destination: Class<T>, extras: Bundle.() -> Unit = {}) {
     val intent = Intent(this, destination)
@@ -27,3 +28,24 @@ suspend fun <T1, T2, R> zip(source1: T1, source2: T2, zipper: (T1, T2) -> R) = c
     async { zipper(source1, source2) }
 }
 
+
+fun View.clicks(
+    scope: CoroutineScope,
+    capacity: Int = Channel.RENDEZVOUS,
+    action: suspend () -> Unit
+) {
+    val events = scope.actor<Unit>(Dispatchers.Main, capacity) {
+        for (unit in channel) action()
+    }
+
+    setOnClickListener(listener(scope, events::offer))
+    events.invokeOnClose { setOnClickListener(null) }
+}
+
+
+private fun listener(
+    scope: CoroutineScope,
+    emitter: (Unit) -> Boolean
+) = View.OnClickListener {
+    if (scope.isActive) { emitter(Unit) }
+}
